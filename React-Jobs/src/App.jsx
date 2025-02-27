04ca06946869d00ef7a70ee9a7f04dadb2535290
Daniel-Clerkson <danielclerkson68@gmail.com>
2025-02-27 16:58:31 +0100

    More Dependencies

--- React-Jobs/src/App.jsx
+++ React-Jobs/src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Homepage from './pages/HomePage'
 import MainLayout from './layout/Main'
-import JobsPage from './layout/Jobs'
+import JobsPage from './pages/Jobs'
+import ErrorPage from './pages/ErrorPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<Homepage />} />  
       <Route path='/jobs' element={<JobsPage />} />  
+      <Route path='*' element={<ErrorPage />} />  
     </Route >
   )
 );
