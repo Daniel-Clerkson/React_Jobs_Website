ede79d80d6ad2adc6cb7bb41c36d624a232b213b
Daniel-Clerkson <danielclerkson68@gmail.com>
2025-02-27 11:25:33 +0100

    Tailwind

--- React-Jobs/src/App.jsx
+++ React-Jobs/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Homecards from './components/Homecards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+);
+
 
 const App = () => {
-  return (
-    <div className='text-5xl'>Hello World</div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
